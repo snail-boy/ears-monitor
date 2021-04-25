fd0744970f6d2a96f671a10544a66b83d1e031f1
王可 <websmallrabbit@163.com>
2021-04-25 11:57:27 +0800

    init

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+// 自定义配置，参考 ./default/setting.config.js，需要自定义的属性在这里配置即可
+module.exports = {
+  fixedHeader: true,
+  multiPage: true,
+  animate: {
+    disabled: false,
+    name: 'fade',
+    direction: 'left'
+  }
+}
+
---
+++ src/pages/monitor/replaySetting/BasicForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-card :body-style="{padding: '24px 32px'}" :bordered="false">
+  </a-card>
+</template>
+
+<script>
+
+export default {
+  name: 'BasicForm',
+  i18n: require('./i18n'),
+  data () {
+    return {
+      value: 1
+    }
+  },
+  computed: {
+    desc() {
+      return this.$t('pageDesc')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
